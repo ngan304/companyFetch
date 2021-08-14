0d925cfd1d3bb8acd01954e046d839e8f5437165
Kim Ngan <nganntk304@gmail.com>
2021-08-14 18:41:03 +1000

    a

---
+++ selenium/javaTester/Topic_06_Fake_Data.java
@@ -0,0 +1,16 @@
+package javaTester;
+
+import java.util.Random;
+
+public class Topic_06_Fake_Data {
+	public static void main(String[] args) {
+		Random rand= new Random();
+		System.out.println(rand.nextInt(9999));
+		
+		
+		//9999 < 10 000
+		//0-9999
+	}
+	
+
+}
