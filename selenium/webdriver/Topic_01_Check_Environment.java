94e93101481ed0fa6959da2efdb85b71494a29f1
Kim Ngan <nganntk304@gmail.com>
2023-01-09 19:35:58 +0700

    q

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -13,41 +13,35 @@ import org.testng.annotations.Test;
 
 public class Topic_01_Check_Environment {
 	WebDriver driver;
-	String projectPath=System.getProperty("user.dir");
+	String projectPath = System.getProperty("user.dir");
+	public String email = "trinhkim6@gmail.com";
+	public String pass = "Trinhkim6#";
 
-	@BeforeClass 
+	@BeforeClass
 	public void beforeClass() {
-		//fire fox
-		System.setProperty("webdriver.gecko.driver", ".\\browserDrivers\\geckodriver.exe"); 
+		System.setProperty("webdriver.gecko.driver", ".\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("http://demo.guru99.com/v4/");
-		
-		// Chrome
-		//System.setProperty("webdriver.chrome.driver", projectPath+"\\browserDrivers\\chromedriver.exe"); 
-		//driver = new ChromeDriver();
-		
-		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		//driver.manage().window().maximize();
-		//driver.get("http://demo.guru99.com/v4/");
+		driver.get("https://vnexpress.net/trong-tre-du-co-ve-lam-to-4545196.html");
 	}
 
 	@Test
-	public void TC_01_ValidateCurrentUrl() {
-		// Login Page Url matching
-		String loginPageUrl = driver.getCurrentUrl();
-		Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+	public void TC_01_CreateNewAccount() {
+		driver.findElement(By.xpath("//a[@title='Đăng nhập']")).click();
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='mfp-iframe iframe_guest']")));
+		sleepInSecond(5);
+		driver.findElement(By.xpath("//a[text()='Tạo tài khoản']")).click();
+		driver.findElement(By.name("myvne_email")).sendKeys(email);
+		driver.findElement(By.name("myvne_password")).sendKeys(pass);
+		driver.findElement(By.id("myvne_button_register")).click();
+
+		// driver.findElement(By.xpath("//input[@id='myvne_email_input']")).sendKeys("Kim");
+		sleepInSecond(5);
+
 	}
 
-	@Test
-	public void TC_02_ValidatePageTitle() {
-		// Login Page title
-		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
-	}
-
-	@Test
+	// @Test
 	public void TC_03_LoginFormDisplayed() {
 		// Login form displayed
 		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
@@ -58,4 +52,11 @@ public class Topic_01_Check_Environment {
 		driver.quit();
 	}
 
+	public void sleepInSecond(long timeoutInSecond) {
+		try {
+			Thread.sleep(timeoutInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
